579cd32606e4681c52cd2c4585848644fd4a7794
Isaac Vander Sluis <isaacvandersluis@gmail.com>
2020-07-27 16:03:12 +0100

    Add update code from Python Robotics

--- fastslam/main.py
+++ fastslam/main.py
@@ -19,6 +19,8 @@ shortcuts.hint()
 
 # --- CODE FROM PYTHON ROBOTICS / ATSUSHI SAKAI ---
 
+# STEP 1: PREDICT
+
 # Fast SLAM covariance
 Q = np.diag([3.0, np.deg2rad(10.0)])**2
 R = np.diag([1.0, np.deg2rad(20.0)])**2
@@ -91,6 +93,178 @@ while SIM_TIME >= time:
     time += DT
     particles = predict_particles(particles, u)
     history.append(deepcopy(particles))
+
+# STEP 2: UPDATE
+
+def observation(xTrue, xd, u, RFID):
+
+    # calc true state
+    xTrue = motion_model(xTrue, u)
+
+    # add noise to range observation
+    z = np.zeros((3, 0))
+    for i in range(len(RFID[:, 0])):
+
+        dx = RFID[i, 0] - xTrue[0, 0]
+        dy = RFID[i, 1] - xTrue[1, 0]
+        d = math.sqrt(dx**2 + dy**2)
+        angle = pi_2_pi(math.atan2(dy, dx) - xTrue[2, 0])
+        if d <= MAX_RANGE:
+            dn = d + np.random.randn() * Qsim[0, 0]  # add noise
+            anglen = angle + np.random.randn() * Qsim[1, 1]  # add noise
+            zi = np.array([dn, pi_2_pi(anglen), i]).reshape(3, 1)
+            z = np.hstack((z, zi))
+
+    # add noise to input
+    ud1 = u[0, 0] + np.random.randn() * Rsim[0, 0]
+    ud2 = u[1, 0] + np.random.randn() * Rsim[1, 1] + OFFSET_YAWRATE_NOISE
+    ud = np.array([ud1, ud2]).reshape(2, 1)
+
+    xd = motion_model(xd, ud)
+
+    return xTrue, z, xd, ud
+
+def update_with_observation(particles, z):
+    for iz in range(len(z[0, :])):
+
+        lmid = int(z[2, iz])
+
+        for ip in range(N_PARTICLE):
+            # new landmark
+            if abs(particles[ip].lm[lmid, 0]) <= 0.01:
+                particles[ip] = add_new_lm(particles[ip], z[:, iz], Q)
+            # known landmark
+            else:
+                w = compute_weight(particles[ip], z[:, iz], Q)
+                particles[ip].w *= w
+                particles[ip] = update_landmark(particles[ip], z[:, iz], Q)
+
+    return particles
+
+def compute_weight(particle, z, Q):
+    lm_id = int(z[2])
+    xf = np.array(particle.lm[lm_id, :]).reshape(2, 1)
+    Pf = np.array(particle.lmP[2 * lm_id:2 * lm_id + 2])
+    zp, Hv, Hf, Sf = compute_jacobians(particle, xf, Pf, Q)
+    dx = z[0:2].reshape(2, 1) - zp
+    dx[1, 0] = pi_2_pi(dx[1, 0])
+
+    try:
+        invS = np.linalg.inv(Sf)
+    except np.linalg.linalg.LinAlgError:
+        print("singuler")
+        return 1.0
+
+    num = math.exp(-0.5 * dx.T @ invS @ dx)
+    den = 2.0 * math.pi * math.sqrt(np.linalg.det(Sf))
+    w = num / den
+
+    return w
+
+def compute_jacobians(particle, xf, Pf, Q):
+    dx = xf[0, 0] - particle.x
+    dy = xf[1, 0] - particle.y
+    d2 = dx**2 + dy**2
+    d = math.sqrt(d2)
+
+    zp = np.array(
+        [d, pi_2_pi(math.atan2(dy, dx) - particle.yaw)]).reshape(2, 1)
+
+    Hv = np.array([[-dx / d, -dy / d, 0.0],
+                   [dy / d2, -dx / d2, -1.0]])
+
+    Hf = np.array([[dx / d, dy / d],
+                   [-dy / d2, dx / d2]])
+
+    Sf = Hf @ Pf @ Hf.T + Q
+
+    return zp, Hv, Hf, Sf
+
+def add_new_lm(particle, z, Q):
+
+    r = z[0]
+    b = z[1]
+    lm_id = int(z[2])
+
+    s = math.sin(pi_2_pi(particle.yaw + b))
+    c = math.cos(pi_2_pi(particle.yaw + b))
+
+    particle.lm[lm_id, 0] = particle.x + r * c
+    particle.lm[lm_id, 1] = particle.y + r * s
+
+    # covariance
+    Gz = np.array([[c, -r * s],
+                   [s, r * c]])
+
+    particle.lmP[2 * lm_id:2 * lm_id + 2] = Gz @ Q @ Gz.T
+
+    return particle
+
+def update_KF_with_cholesky(xf, Pf, v, Q, Hf):
+    PHt = Pf @ Hf.T
+    S = Hf @ PHt + Q
+
+    S = (S + S.T) * 0.5
+    SChol = np.linalg.cholesky(S).T
+    SCholInv = np.linalg.inv(SChol)
+    W1 = PHt @ SCholInv
+    W = W1 @ SCholInv.T
+
+    x = xf + W @ v
+    P = Pf - W1 @ W1.T
+
+    return x, P
+
+def update_landmark(particle, z, Q):
+
+    lm_id = int(z[2])
+    xf = np.array(particle.lm[lm_id, :]).reshape(2, 1)
+    Pf = np.array(particle.lmP[2 * lm_id:2 * lm_id + 2, :])
+
+    zp, Hv, Hf, Sf = compute_jacobians(particle, xf, Pf, Q)
+
+    dz = z[0:2].reshape(2, 1) - zp
+    dz[1, 0] = pi_2_pi(dz[1, 0])
+
+    xf, Pf = update_KF_with_cholesky(xf, Pf, dz, Q, Hf)
+
+    particle.lm[lm_id, :] = xf.T
+    particle.lmP[2 * lm_id:2 * lm_id + 2, :] = Pf
+
+    return particle
+
+# END OF CODE SNIPPET #
+
+
+
+# Setting up the landmarks
+RFID = np.array([[10.0, -2.0],
+                [15.0, 10.0]])
+N_LM = RFID.shape[0]
+
+# Initialize 1 particle
+N_PARTICLE = 1
+particles = [Particle(N_LM) for i in range(N_PARTICLE)]
+
+xTrue = np.zeros((STATE_SIZE, 1))
+xDR = np.zeros((STATE_SIZE, 1))
+
+print("initial weight", particles[0].w)
+
+xTrue, z, _, ud = observation(xTrue, xDR, u, RFID)
+# Initialize landmarks
+particles = update_with_observation(particles, z)
+print("weight after landmark initialization", particles[0].w)
+particles = update_with_observation(particles, z)
+print("weight after update ", particles[0].w)
+
+particles[0].x = -10
+particles = update_with_observation(particles, z)
+print("weight after wrong prediction", particles[0].w)
+
+# STEP 3: RESAMPLE
+
+
 
 # --- END CODE FROM PYTHON ROBOTICS / ATSUSHI SAKAI ---
 
