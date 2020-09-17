1ea35d10b943c19a6c15fb2b26473ffd2446410e
Isaac Vander Sluis <isaacvandersluis@gmail.com>
2020-09-17 11:45:57 +0100

    Add plot title

--- fastslam/main.py
+++ fastslam/main.py
@@ -628,6 +628,7 @@ class Listener(BaseListener):
         plt.plot(self.hxEst[0, :], self.hxEst[1, :], "-r", label='Est. Path') # Plot xEst with solid red line
         plt.plot(self.xEst[0], self.xEst[1], "xk", label='Est. Pose') # Plot current xEst as black x
         plt.legend()
+        plt.title('FastSLAM 1.0')
         plt.xlabel('X distance (m)')
         plt.ylabel('Y distance (m)')
         plt.axis("equal")
